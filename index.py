756a9e23113f2836dbffc5f8ebe572cedbedf4c8
andyboli <webdevelopmentbolivar@gmail.com>
2023-02-28 18:33:20 -0300

    start and restart application

--- index.py
+++ index.py
@@ -1,3 +1,40 @@
+import inquirer
+
+from controller.orchestrator import restart_app, start_app
+
+
+questions = [
+    inquirer.List('leave',
+                  message="Kill the program?",
+                  choices=['Yes', 'No'],
+                  ),
+]
+
 
 if __name__ == "__main__":
-    print("Starting Puc Project 02")
+    try:
+        while True:
+            try:
+                success, loading, error = next(start_app)
+                if loading:
+                    print(loading)
+                elif success:
+                    print(success)
+                elif error:
+                    print(error)
+            except StopIteration:
+                answers = inquirer.prompt(questions)
+                if answers['leave'] == 'Yes':
+                    break
+    finally:
+        while True:
+            try:
+                success, loading, error = next(restart_app)
+                if loading:
+                    print(loading)
+                elif success:
+                    print(success)
+                elif error:
+                    print(error)
+            except StopIteration:
+                break
