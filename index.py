b262a03ff0e8c78c23a5f49a809865c876a01da1
andyboli <webdevelopmentbolivar@gmail.com>
2023-04-30 19:32:33 -0300

    add view

--- index.py
+++ index.py
@@ -1,40 +1,6 @@
-import inquirer
-
-from controller.orchestrator import restart_app, start_app
-
-
-questions = [
-    inquirer.List('leave',
-                  message="Kill the program?",
-                  choices=['Yes', 'No'],
-                  ),
-]
+""" from controller.orchestrator import restart_app, start_app """
+from view import main_menu
 
 
 if __name__ == "__main__":
-    try:
-        while True:
-            try:
-                success, loading, error = next(start_app)
-                if loading:
-                    print(loading)
-                elif success:
-                    print(success)
-                elif error:
-                    print(error)
-            except StopIteration:
-                answers = inquirer.prompt(questions)
-                if answers['leave'] == 'Yes':
-                    break
-    finally:
-        while True:
-            try:
-                success, loading, error = next(restart_app)
-                if loading:
-                    print(loading)
-                elif success:
-                    print(success)
-                elif error:
-                    print(error)
-            except StopIteration:
-                break
+    main_menu.build_main_menu()
---
+++ view/main_menu.py
@@ -0,0 +1,27 @@
+import inquirer
+from inquirer.themes import GreenPassion
+
+from controller import reader
+from view import register_menu, utils
+
+
+main_menu_choices = {
+    "register_restaurant": reader.read_text(
+        "menu_register_restaurant"),
+    "exit_app": reader.read_text(
+        "menu_exit_app")
+}
+
+main_menu_key = 'main_menu_choices'
+
+
+def build_main_menu() -> str:
+    utils.build_menu_header()
+    answers = inquirer.prompt(
+        [inquirer.List(main_menu_key,
+                       message=reader.read_text("menu_choose_action"),
+                       choices=main_menu_choices.values())],
+        theme=GreenPassion())
+    answer = answers[main_menu_key]
+    if answer == main_menu_choices["register_restaurant"]:
+        return register_menu.build_register_menu()
